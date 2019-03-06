be424481a136033f88ba0bb22acba08863fe2d58
Ivan Apet <van.ka515@gmail.com>
2019-03-06 17:18:37 +0300

    Initial

---
+++ Podfile
@@ -0,0 +1,18 @@
+
+
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+use_frameworks!
+
+target 'gplaceTest' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+
+  
+  pod 'GooglePlacePicker'
+  
+  pod 'GoogleMaps'
+  pod 'GooglePlaces'
+end
+
+
---
+++ gplaceTest/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  gplaceTest
+//
+//  Created by Andrey Apet on 3/6/19.
+//  Copyright © 2019 apetIvan. All rights reserved.
+//
+
+import UIKit
+import GooglePlaces
+import GooglePlacePicker
+
+class ViewController: UIViewController {
+
+    var placesClient: GMSPlacesClient!
+    
+    // Add a pair of UILabels in Interface Builder, and connect the outlets to these variables.
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        placesClient = GMSPlacesClient.shared()
+    }
+    
+    
+    @IBAction func pickPlace(_ sender: UIButton) {
+        let config = GMSPlacePickerConfig(viewport: nil)
+        let placePicker = GMSPlacePickerViewController(config: config)
+        placePicker.delegate = self
+        
+        
+        present(placePicker, animated: true, completion: nil)
+    }
+    
+    // To receive the results from the place picker 'self' will need to conform to
+    // GMSPlacePickerViewControllerDelegate and implement this code.
+    func placePicker(_ viewController: GMSPlacePickerViewController, didPick place: GMSPlace) {
+        // Dismiss the place picker, as it cannot dismiss itself.
+        viewController.dismiss(animated: true, completion: nil)
+        
+        print("Place name \(place.name)")
+        print("Place address \(place.formattedAddress)")
+        print("Place attributions \(place.attributions)")
+    }
+    
+    func placePickerDidCancel(_ viewController: GMSPlacePickerViewController) {
+        // Dismiss the place picker, as it cannot dismiss itself.
+        viewController.dismiss(animated: true, completion: nil)
+        
+        print("No place selected")
+    }
+
+}
+
+extension ViewController: GMSPlacePickerViewControllerDelegate {
+    
+}
